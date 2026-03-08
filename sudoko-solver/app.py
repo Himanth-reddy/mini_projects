1f7d4ce1425007de922b203a40554baece3a0542
Himanth Reddy <himanth421@gmail.com>
2026-03-08 18:18:44 +0530

    Fetch Sudoku puzzle from API

--- sudoko-solver/app.py
+++ sudoko-solver/app.py
@@ -1,3 +1,6 @@
+import requests
+response = requests.get("https://sudoku-api.vercel.app/api/dosuku")
+board = response.json()["newboard"]["grids"][0]["value"]
 '''solve(board):
     find empty cell
     if none → solved
@@ -56,17 +59,7 @@ def print_board(board):
             print(board[i][j], end=" ")
         print()
 
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+
 if solve(board):
     print("\nSolved Sudoku:")
     print_board(board)
